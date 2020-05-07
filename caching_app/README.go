6e2591c83dd4567d91839f54523aebe6caef2c6c
ManuManjunath <52904203+ManuManjunath@users.noreply.github.com>
2020-05-07 12:19:30 +0530

    Update README.go

--- caching_app/README.go
+++ caching_app/README.go
@@ -4,3 +4,6 @@ main.go generates a random number between 1 and 10.
 Based on this number, the movie with the given rank is displayed. 
 This random generation of number between 1 and 10 occurs 10 times. 
 If a certain rank is queried for the first time, it picks the data from the database. This data is also cached, which is where the data is pulled from if the same rank is queried again.
+
+To run the code - 
+$ go run .
