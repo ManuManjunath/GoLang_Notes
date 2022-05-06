a734cf2a3bbe5f37617c09c8fd2c39d7da78098c
ManuManjunath <52904203+ManuManjunath@users.noreply.github.com>
2022-05-06 13:09:16 +0530

    Update influx_extract.go

--- influx_extract.go
+++ influx_extract.go
@@ -11,3 +11,76 @@ cluster --> Cluster Name
 For example:
 go run patroniPreChecks.go -c cxxx12345 -e exxx12345 -cluster myDB
 */
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/jedib0t/go-pretty/table"
+	_ "github.com/lib/pq"
+)
+
+const (
+	url  = "https://xxxxinfluxxxxx-postgres.example.com/query?q="
+	pg_port = 5432
+	pg_user = "userID"
+	pg_pass = "REDACTED"
+	pg_ssl  = "require"
+)
+
+type PullMetrics struct {
+	Results []Results
+}
+
+type Results struct {
+	Series []Series
+}
+
+type Series struct {
+	Name    string
+	Tags    map[string]string
+	Columns []string
+	Values  [][]interface{}
+}
+
+type CurrentObjects struct {
+	TableCatalog string
+	TableName    string
+	TableType    string
+}
+
+func queryInflux(qry string) ([]byte, error) {
+	resp, err := http.Get(url + url.QueryEscape(qry))
+	if err != nil {
+		fmt.Printf("ERROR: Unable to run %v. %v\n", qry, err)
+		return nil, errors.New("Unable to query from Influx: " + qry)
+	} else {
+		data, _ := ioutil.ReadAll(resp.Body)
+		return data, nil
+	}
+}
+
+func getComplianceType(cmpl string) string {
+	switch {
+	case cmpl == "0":
+		return "None"
+	case cmpl == "1":
+		return "ComplianceType1"
+	case cmpl == "2":
+		return "ComplianceType2"
+	case cmpl == "3":
+		return "ComplianceType3"
+	case cmpl == "4":
+		return "ComplianceType4"
+	default:
+		return "Unknown"
+	}
+}
